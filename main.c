df7386a7ea15bad23deb40b8cb0e1399d4c2a447
Abramov Yuri <murloc228.murloc228@yandex.ru>
2017-03-15 23:48:48 +0300

    New branch.

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char ** argv)
 {
-	printf("Hello world!\n");
+	char first[255], last[255];
+	
+	printf("Enter your first name: ");
+	fgets(first, 255, stdin);
+	first[strlen(first) - 1] = '\0';
+	
+	printf("Now enter your last name: ");
+	gets(last);
+	
+	printf("Hello %s %s!\n", first, last);
 	return 0;
 }
